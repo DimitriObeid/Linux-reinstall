089b16c97fbd6fe94a5ecfff1e63edac3a0a6e8c
Dimitri Obeid <obeiddimitri@gmail.com>
2019-11-27 19:06:49 +0100

    Versio 1.1 en approche

--- README.md
+++ README.md
@@ -13,41 +13,49 @@ La lecture de la documentation est vivement conseillée, surtout si vous avez be
 
 ## 2) Description des fichiers du projet
 
-Deux sous-dossiers se trouvent dans ce repository :  
-  
-- *__Réinstallation personnelle :__* Contient mon script de réinstallation personnelle. Vous pouvez le télécharger si vous souhaitez installer les mêmes programmes que moi ou plus que le strict minimum pour le BTS SIO.  
-- *__V 1.0 :__* Ne contient que le strict minimum dont vous avez besoin pour vos cours de BTS SIO.  
+Dans ce dépôt Github se trouvent 
 
-* ### 2.a) Dossier _"Bêta"_ :  
+Deux sous-dossiers se trouvent dans ce dépôt :  
+- *Bêta :__* 
+    - Contient 
+
+  
+- *__V 1.0 :__* 
+    - Contient le strict minimum dont vous avez besoin pour vos cours de BTS SIO.  
+    - Contient également mon script de réinstallation personnelle. Vous pouvez le télécharger si vous souhaitez installer les mêmes programmes que moi ou plus que le strict minimum pour le BTS SIO.  
+    - Entre les deux scripts, les seuls changements se situent au niveau des logiciels à installer. Le script personnalisé ajoute les mêmes programmes que le script minimal, plus de nombreux programmes supplémentaires.
+  
+## 3) Contenu du dossier actuel et des sous-dossiers
+  
+
+* ### 3.a) Dossier _"Bêta"_ :  
     - *__Documentation Bêta.odt :__* La documentation de la version Bêta du projet. Elle est régulièrement mise à jour.  
     - *__beta.sh :__* Le fichier de réinstallation en version Bêta. Il est régulièrement mis à jour.
 
-* ### 2.b) Dossier _"Personnel"_ :
-    - *__Documentation personnalisée.odt :__* La documentation officielle de ma version de réinstallation personnalisée  
-    - *__personnel.sh :__* Mon fichier de réinstallation personnelle. Il est mis à jour lorsqu'une version stable du fichier de réinstallation bêta sort  
 
-* ### 2.c) Dossier _"V 1.0"_ :
+* ### 3.b) Dossier _"V 1.0"_ :
+    - *__Documentation personnalisée.odt :__* La documentation officielle de ma version de réinstallation personnalisée  
     - *__Documentation version 1.0.odt :__* La documentation officielle de la version 1.0 du script.  
     - *__Linux Reinstall 1.0.sh :__* LE fichier de la version 1.0 du script que vous souhaitez exécuter pour réinstaller vos programmes.  
+    - *__personnel.sh :__* Mon fichier de réinstallation personnelle. Il est mis à jour lorsqu'une version stable du fichier de réinstallation bêta sort  
 
 
-* ### 2.d) Dossier actuel :
+* ### 3.c) Dossier actuel :
     - *__Bug tracking.md :__* Liste tous les bugs connus jusqu'à leur correction.  
     - *__Changelogs.md :__* Contient les changements ayant eu lieu entre deux versions.  
-    - *__Mode d'emploi.odt :__* Mode d'emploi du script
+    - *__Mode d'emploi.odt :__* Mode d'emploi d'exécution de chaque script
     - *__README.md :__* Le fichier que vous lisez actuellement sur Github (ou dans tout autre lecteur de fichiers Markdown).  
 
-## 3) Fichiers à télécharger :
+## 4) Fichiers à télécharger :
 
-- Les fichiers qui vous intéresseront le plus pour vos cours de BTS SIO sont les deux fichiers se trouvant dans le dossier **"V 1.0"**.  
-- Les fichiers qui vous intéresseront le plus pour une utilisation personnelle ou ou plus avancée se trouvent dans le dossier **"Personnel"**.  
+- Les fichiers qui vous intéresseront le plus pour vos cours de BTS SIO ou pour une utilisation personnelle ou ou plus avancée sont les quatre fichiers se trouvant dans le dossier **"V 1.0"**.  
 - Les fichiers qui vous intéresseront le plus si vous êtes développeur et que vous souhaitez participer à l'élaboration du projet se trouvent dans le dossier **"Bêta"**.  
 
-## 4) Mode d'emploi
+## 5) Mode d'emploi
 
 Lisez le fichier **"Mode d'emploi.odt"** pour voir comment exécuter un des scripts.
 
-# 5) Ajouts futurs
+# 6) Ajouts futurs
 
 Téléchargement de VMware  
-Ajout de sudo et configuration pour y accéder en tant qu'utilisateur normal pour Debian
+Ajout de sudo et configuration pour y accéder en tant qu'utilisateur normal pour Debian.__
--- tests.sh
+++ tests.sh
@@ -1,92 +1,88 @@
 #!/bin/bash
 
-## COULEURS
-# Couleurs pour mieux lire les étapes de l'exécution du script
-C_ROUGE=$(tput setaf 196)   # Rouge clair
-C_VERT=$(tput setaf 82)     # Vert clair
-C_JAUNE=$(tput setaf 226)   # Jaune clair
-C_RESET=$(tput sgr0)        # Restaurer la couleur originale du texte affiché selon la configuration du profil du terminal
-C_HEADER_LINE=$(tput setaf 6)      # Bleu cyan. Définition de l'encodage de la couleur du texte du header. /!\ Ne modifier l'encodage de couleurs du header qu'ici ET SEULEMENT ici /!\
-
-## AFFICHAGE DE TEXTE
-# Nombre de chevrons avant les chaînes de caractères vertes et rouges
-TAB=">>>>"
-J_TAB="$C_JAUNE$TAB"
-R_TAB="$C_ROUGE$TAB$TAB"
-V_TAB="$C_VERT$TAB$TAB"
-# En cas de mauvaise valeur rentrée avec un "read"
-READ_VAL="$R_TAB Veuillez rentrer une valeur valide [oui, non] $C_RESET"
-
-get_dist_package_manager()
+draw_header_line()
 {
-	echo "$J_TAB Détection de votre gestionnaire de paquet :$C_RESET"
-
-    which zypper &> /dev/null && OS_FAMILY="opensuse"
-    which pacman &> /dev/null && OS_FAMILY="archlinux"
-    which dnf &> /dev/null && OS_FAMILY="fedora"
-    which apt-get &> /dev/null && OS_FAMILY="debian"
-    which emerge &> /dev/null && OS_FAMILY="gentoo"
-
-	# Si, après l'appel de la fonction, la string contenue dans la variable $OS_FAMILY est toujours nulle
-	if [ "$OS_FAMILY" = "void" ]; then
-		echo "$ERROR_OUTPUT_1 ERREUR FATALE : LE GESTIONNAIRE DE PAQUETS DE VOTRE DISTRIBUTION N'EST PAS SUPPORTÉ !!!$ERROR_OUTPUT_2"
-		exit 1
-	else
-		echo "$V_TAB Le gestionnaire de paquets de votre distribution est supporté ($OS_FAMILY) $C_RESET"; echo ""
+	cols=$(tput cols)
+	char=$1
+	color=$2
+	if test "$color" != ""; then
+		echo -ne $color
+	fi
+	for i in $(eval echo "{1..$cols}"); do
+		echo -n $char
+	done
+	if test "$color" != ""; then
+		echo -ne $C_RESET
 	fi
 }
 
-pack_install()
+# Affichage du texte des headers
+script_header()
 {
-    package_name=$@
-	case $OS_FAMILY in
-		opensuse)
-			echo "$V_TAB Installation de $package_name$C_RESET"
-			$SLEEP_INST
-			zypper -y install $package_name
-			;;
-		archlinux)
-            echo "$V_TAB Installation de $package_name$C_RESET"
-			$SLEEP_INST
-			pacman --noconfirm -S $package_name
-			;;
-		fedora)
-            echo "$V_TAB Installation de $package_name$C_RESET"
-			$SLEEP_INST
-			dnf -y install $package_name
-			;;
-		debian)
-			echo "$V_TAB Installation de $package_name$C_RESET"
-			$SLEEP_INST
-			apt-get -y install $package_name
-			echo "$J_TAB Détection de la réussite de l'installation $C_RESET"
-			if ! which $pack_name > /dev/null; then
-				echo "$R_TAB Échec de l'installation de $pack_name $C_RESET"
-			else
-				echo "$V_TAB Succès de l'installation de $pack_name  $C_RESET"
-			fi
-			;;
-		gentoo)
-			echo "$V_TAB Installation de $package_name$C_RESET"
-			$SLEEP_INST
-			emerge $package_name
-			;;
-	esac
-    echo ""
+	color=$2
+	if test "$color" = ""; then
+        # Définition de la couleur des lignes
+		color=$C_HEADER_LINE
+	fi
+
+	# Décommenter la ligne ci dessous pour activer le chronomètre avant l'affichage du header
+#    $SLEEP
+	echo -ne $color    # Afficher la ligne du haut selon la couleur de la variable $color
+	draw_header_line $LINE_CHAR
+    # Commenter la ligne du dessous pour que le prompt "##>" soit de la même couleur que la ligne du dessus
+#    echo -ne $C_RESET
+	echo "##> "$1
+	draw_header_line $LINE_CHAR
+	echo -ne $color
+	$SLEEP_TAB
 }
 
+LINE_CHAR="-"
+C_ROUGE=$(tput setaf 196)
+C_VERT=$(tput setaf 82)
+C_JAUNE=$(tput setaf 226)
+C_RESET=$(tput sgr0)
+C_HEADER_LINE=$(tput setaf 6)
 
-snap_install()
+draw_header_line_test()
 {
-    snap_name=$@
-    which $snap_name > /dev/null
-	if 
-    snap install $snap_name
+    cols=$(tput cols)
+	char=$1
+	color=$2
+	if test "$color" != ""; then
+		echo -ne $color
+	fi
+	for i in $(eval echo "{1..$cols}"); do
+		echo -n $char
+	done
+	if test "$color" != ""; then
+		echo -ne $C_RESET
+	fi
 }
 
+script_header_test()
+{
+#	end_color="$C_HEADER_LINE"
+#	$end_color=$3
+    color=$2
+	if test "$color" = ""; then
+        # Définition de la couleur des lignes
+		color=$C_HEADER_LINE
+	fi
 
-pack_install tree
-snap_install discord
+	echo ""
+	# Décommenter la ligne ci dessous pour activer le chronomètre avant l'affichage du header
+#    $SLEEP
+	echo -ne $color    # Afficher la ligne du haut selon la couleur de la variable $color
+	draw_header_line_test $LINE_CHAR
+    # Commenter la ligne du dessous pour que le prompt "##>" soit de la même couleur que la ligne du dessus
+#    echo -ne $C_RESET
+	echo "##> "$1 $color
+	draw_header_line_test $LINE_CHAR
+	echo -ne $C_RESET
+	echo ""
+	$SLEEP_TAB
+}
 
-
-
+script_header_test "TEST"
+echo "test"
