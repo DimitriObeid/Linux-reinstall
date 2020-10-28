4b54e700783b557a998c7633853b158bd68f02c1
Dimitri <obeiddimitri@gmail.com>
2020-10-28 10:06:08 +0100

    Sauvegarde

--- GUI/Main.py
+++ GUI/Main.py
@@ -2,6 +2,7 @@
 
 # *** Importing Tkinter modules
 from tkinter import *
+import tkinter as tk
 # from tkinter import ttk
 
 
@@ -9,19 +10,49 @@ from tkinter import *
 # from Translate import welcomeGUI*
 
 # *** Code
-def main():
-    app = Tk()  # Window creation
+# * Window creation
+app = tk.Tk()
 
-    # Defining window geometry
-    # Window dimensions must be written as a string, as a single argument.
-    # The multiplier isn't the star character '*', but the latin alphabet character 'x'
-    app.geometry('300x300')
-    app['bg'] = 'black'
-
-    # Program's main loop
-    app.mainloop()
-
-    exit(0)
+# * Defining window geometry
+# Window dimensions must be written as a string, as a single argument.
+# The multiplier isn't the star character '*', but the latin alphabet character 'x'
+app.geometry('300x300')
+app['bg'] = 'black'
 
 
-main()
+# * Creating frames to place the widgets esthetically
+# For the action buttons
+frame_btns = Frame(app, bd=1, relief=SUNKEN)
+frame_btns.pack(padx=5, pady=5)
+
+# For the language choice button
+frame_lang = Frame(app, bd=1, relief=SUNKEN)
+frame_lang.pack(padx=5, pady=5)
+
+# For the welcoming text
+frame_txt = Frame(app, bd=1, relief=SUNKEN)
+frame_txt.pack(padx=5, pady=5)
+
+
+# * Creating buttons
+# Adding packages
+btn_add = Button(frame_btns, text='Ajouter')
+btn_add.pack(side=tk.BOTTOM)
+
+# Language
+btn_lang = Button(frame_lang, text='Langu(ag)e')
+btn_lang.pack(side=tk.TOP)
+
+# Quit
+btn_quit = Button(frame_btns, text='Quitter', command=app.destroy)
+btn_quit.pack(side=tk.LEFT)
+
+# Validate
+btn_valid = Button(frame_btns, text='Valider')
+btn_valid.pack(side=tk.LEFT)
+
+
+# * Program's main loop
+app.mainloop()
+
+exit(0)
--- GUI/Translate.py
+++ GUI/Translate.py
@@ -7,23 +7,70 @@ import csv
 # *** Variables
 TRANSLF = 'Translations.csv'     # Translation file
 
+# * Language
+default_lang = 'fr'
+current_lang = default_lang
+
+'''
+# * CSV file translations
+# Adding package window
+Add packages window = GetTranslateCSVInfos()
+addPackGUI_BtnAddPack = GetTranslateCSVInfos()
+addPackGUI_BtnAddPackMsg = GetTranslateCSVInfos()
+addPackGUI_BtnAddPPA = GetTranslateCSVInfos()
+addPackGUI_BtnAddPPAMsg = GetTranslateCSVInfos()
+addPackGUI_Cancel = GetTranslateCSVInfos()
+addPackGUI_CancelMsg = GetTranslateCSVInfos()
+
+# Main menu window
+welcomeGUI_BntAdd = GetTranslateCSVInfos()
+welcomeGUI_BtnAddMsg = GetTranslateCSVInfos()
+welcomeGUI_BtnQuit = GetTranslateCSVInfos()
+welcomeGUI_BtnQuitMsg = GetTranslateCSVInfos()
+welcomeGUI_BtnVal = GetTranslateCSVInfos()
+welcomeGUI_BtnValMsg = GetTranslateCSVInfos()
+welcomeGUI_WinMsg1 = GetTranslateCSVInfos()
+welcomeGUI_WinMsg2 = GetTranslateCSVInfos()
+welcomeGUI_WinName = GetTranslateCSVInfos()
+'''
+
 # *** Code
-# def GetRow(target, word):
 
 
-def GetCSVCol(target, col):
-    if col > 2:
-        print("Error : Invalid column index")
+# This function search the matching column and row
+def GetTranslateCSVInfos(col, row):
+    if path.exists(TRANSLF):
+        with open(TRANSLF, 'r') as csv_file:
+            # Columns
+            csv_col_reader = csv.DictReader(csv_file, delimiter=',')
+            verif_col = ['fr', 'en']
+
+            # Rows
+            csv_row_reader = csv.DictReader(csv_file, delimiter='\n')
+            verif_row = row
+
+            # Getting columns first
+            if col in verif_col:
+                for lines_c in csv_col_reader:
+                    print(lines_c[col])
+
+                    # Getting row last
+                    if row in verif_row:
+                        for lines_r in csv_row_reader:
+                            print(lines_r[row])
+                    else:
+                        print("One of the CSV file's row name doesn't match to an expected value")
+                        csv_file.close()
+                        exit(84)
+                csv_file.close()
+
+            else:
+                print("One of the CSV file's column name doesn't match to an expected value")
+                csv_file.close()
+                exit(84)
+    else:
+        print("Error : The file", TRANSLF, "doesn't exists")
         exit(84)
-    with open(target, 'r', newline='') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-
-        for lines in csv_reader:
-            print(lines[col])
 
 
-if path.exists(TRANSLF):
-    GetCSVCol(TRANSLF, 3)
-else:
-    print("Error : The file", TRANSLF, "doesn't exists")
-    exit(84)
+GetTranslateCSVInfos('fr', 'welcomeGUI_WinMsg1')
