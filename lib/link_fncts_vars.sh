cdc6d5a554b538c337fd46c0ee75ab2a392e34f1
Dimitri <obeiddimitri@gmail.com>
2020-11-22 23:36:44 +0100

    Sauvegarde

--- lib/link_fncts_vars.sh
+++ lib/link_fncts_vars.sh
@@ -2,6 +2,12 @@
 
 # This script was made to avoid oversizing the main script with all the files sourcing.
 
-for _ in "$MAIN_PROJECT_ROOT/$MAIN_L_FNCTS"; do
+# Including variables first.
+for _ in "$MAIN_PROJECT_ROOT/$MAIN_L_VARS"; fo
+    source "$MAIN_PROJECT_ROOT/$MAIN_L_VARS/*.var" || echo "$_ : not found"
+done
 
+# Including functions next.
+for _ in "$MAIN_PROJECT_ROOT/$MAIN_L_FNCTS"; do
+    source "$MAIN_PROJECT_ROOT/$MAIN_L_FNCTS/*.lib" || echo "$_ : not found"
 done
